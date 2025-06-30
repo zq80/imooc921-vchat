99e48c40a186826a608284897fdd87d0d5d6c47b
zeng qing <50618591@qq.com>
2025-06-30 22:19:17 +0800

    6-2 添加vue3支持 https://www.electronforge.io/guides/framework-integration/vue-3

--- src/renderer.ts
+++ src/renderer.ts
@@ -29,3 +29,8 @@
 import './index.css';
 
 console.log('👋 This message is being logged by "renderer.ts", included via Vite');
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,10 @@
 import { defineConfig } from 'vite';
 
+
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig(async () => {
+    const vue = (await import('@vitejs/plugin-vue')).default
+    return {
+        plugins: [vue()]
+    }
+});
