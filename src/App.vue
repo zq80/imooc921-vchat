758a3eacf02bece9b95a7e1995d2e986df29895c
zeng qing <50618591@qq.com>
2025-07-01 01:43:19 +0800

    6-4

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
-  <h1 class=" text-3xl font-bold bg-sky-300">💖 Hello World!</h1>
-  <p>Welcome to your Electron application.</p>
+  <div class=" flex items-center justify-between h-screen">
+    <div class=" bg-gray-300 text-gray-700 w-1/2 h-full hover:bg-yellow-300">sidebar</div>
+    <div class=" bg-green-300 text-blue-700 h-full flex-1">
+      content
+      <input type="text" placeholder="search for anything" class=" placeholder:text-blue-300">
+    </div>
+  </div>
 </template>
 
 <script setup>
--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,8 @@ if (started) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1024,
+    height: 768,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
