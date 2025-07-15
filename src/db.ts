007ee0ff460a4bd47405e277cb6443f05e31b624
zeng qing <50618591@qq.com>
2025-07-15 08:12:22 +0800

    9-3

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,24 @@
 import Dexie, { type EntityTable } from "dexie";
-import { ProviderProps } from "./type";
+import { providers } from "./testData";
+import { ProviderProps, ConversationProps, MessageProps } from "./type";
 
 export const db = new Dexie('vChatDatabase') as Dexie & {
     providers: EntityTable<ProviderProps, 'id'>;
+    conversations: EntityTable<ConversationProps, 'id'>;
+    messages: EntityTable<MessageProps, 'id'>;
 
 }
 
 db.version(1).stores({
-    providers: '++id,name'
+    providers: '++id,name',
+    conversations: '++id,providerId',
+    messages: '++id,conversationId'
 })
+
+export const initProviders = async () => {
+    const count = await db.providers.count()
+    if (count === 0) {
+        db.providers.bulkAdd(providers)
+    }
+
+}
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,52 @@
 <template>
   <div class="w-[80%] mx-auto h-full">
     <div class="flex items-center h-[85%]">
-      <ProviderSelect :items="providers" v-model="selectedModel"/>
+      <ProviderSelect :items="providers" v-model="currentProvider"/>
     </div>
     <div class="flex items-center h-[15%]">
-      <MessageInput />
+      <MessageInput @create="createConversation" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-import { ProviderProps } from '../types'
+import { computed, onMounted, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { db } from '../db'
+// import { ProviderProps } from '../types'
 import ProviderSelect from '../components/ProviderSelect.vue'
 import MessageInput from '../components/MessageInput.vue'
-const selectedModel = ref('')
-
-const providers: ProviderProps[] = [
-  {     
-    id: 1,
-    name: '文心一言', 
-    desc: '文心一言 百度出品的大模型',
-    models: ['ERNIE-4.0-8K', 'ERNIE-3.5-8K', 'ERNIE-Speed-8K'],
-    avatar: 'https://qph.cf2.poecdn.net/main-thumb-pb-3004-50-jougqzjtwfqfyqprxbdwofvnwattmtrg.jpeg',
-    createdAt: '2024-07-03',
-    updatedAt: '2024-07-03'
-  },
-  {     
-    id: 2,
-    name: '通义千问', 
-    desc: '通义千问',
-    // https://help.aliyun.com/zh/dashscope/developer-reference/api-details?spm=a2c4g.11186623.0.0.5bf41507xgULX5#b148acc634pfc
-    models: ['qwen-turbo', 'qwen-plus', 'qwen-max'],
-    avatar: 'https://qph.cf2.poecdn.net/main-thumb-pb-3004-50-jougqzjtwfqfyqprxbdwofvnwattmtrg.jpeg',
-    createdAt: '2024-07-03',
-    updatedAt: '2024-07-03'
+import { ProviderProps } from 'src/type'
+const currentProvider = ref('')
+const router=useRouter()
+const providers=ref<ProviderProps[]>([])
+onMounted(async()=>{
+  providers.value=await db.providers.toArray()
+})
+const modelInfo=computed(()=>{
+  const [providerId,selectedModel]=currentProvider.value.split('/')
+  return{
+    providerId:parseInt(providerId),
+    selectedModel
   }
-]
-console.log('👋 This message is being logged by "App.vue", included via Vite');
+})
+const createConversation=async(question:string)=>{
+  const {providerId,selectedModel}=modelInfo.value
+  const currentDate=new Date().toISOString()
+  const conversationId = await db.conversations.add({
+    title:question,
+    providerId,
+    selectedModel,
+    createdAt:currentDate,
+    updatedAt:currentDate
+  })
+  const newMessageId = await db.messages.add({
+    content:question,
+    conversationId,
+    createdAt:currentDate,
+    updatedAt:currentDate,
+    type:"question"
+  })
+  router.push(`/conversation/${conversationId}?init=${newMessageId}`)
+}
 </script>
